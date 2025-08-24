6df21a665672b45f6e94d65d2032bb4a721fa4ff
Alice <ormanalis2007@gmail.com>
2025-08-24 16:55:35 +0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
-# def main():
-#
+import screen
+from screen import default_window
+
+open_window=True
+def main():
+    pygame.init()
+    default_window()
+    while open_window:
+        arrow_direction=pygame.key.get_pressed()
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                open_window=False
+            elif k
+
