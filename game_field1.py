83260cab423108edd349f83f47670ec0f902a7d8
Dana-Ezra <zinaezra@gmail.com>
2025-08-25 10:17:18 +0300

    bluh

--- game_field1.py
+++ game_field1.py
@@ -1,9 +1,4 @@
 import random
-from glob import translate
-from math import floor
-from xml.sax.handler import property_xml_string
-
-from pygame.examples.scrap_clipboard import screen
 game_field = []
 
 def create_matrix(matrix):
@@ -15,37 +10,35 @@ def create_matrix(matrix):
         list1 = []
     return matrix
 
-
-def from_matrix_screen(matrix):
-    dict_x = {}
-    dict_y = {}
-    from screen import SCREEN
-    for x in SCREEN:
-        index_x = floor(x/10-1)
-        dict_x[x]=index_x
-    for y in screen:
-        index_y = floor(y/10-1)
-        dict_y[y]= index_y
-    return dict_x,dict_y
-
-#
-# def dict_value(matrix1):
-#     dict1 = {}
-#     matrix = create_matrix(game_field)
-#     x = from_matrix_screen(matrix)
-#     for i in range(len(matrix)):
-#         for j in range(len(matrix[i])):
-#             if  i in x[0].values():
-#                 if j in x[1].values():
-#                     [i][j] =
-
+game_field = create_matrix(game_field)
 
 def flag_in_field(matrix):
     for i in range(len(matrix)):
       for j in range(len(matrix[i])):
-          if i>=20:
-              if j>=46:
-                  matrix[i][j]="Flag"
+          if i>=20 and j>=46:
+             matrix[i][j]="Flag"
+    return matrix
 
-flag_in_field(game_field)
+game_field = flag_in_field(game_field)
+
+def soldier_in_board(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if i<=3 and j<=1:
+              matrix[i][j]="Soldier"
+    return matrix
+
+
+game_field = soldier_in_board(game_field)
+
+def mine_in_board(matrix):
+ for i in range(20):
+    x = random.randint(0,25)
+    y = random.randint(0,50)
+    if not x==0 and not y==0 and matrix[x][y]!="Flag" and matrix[x][y]!="Soldier":
+      matrix[x][y]="Mine"
+ return matrix
+
+game_field = mine_in_board(game_field)
+
 
