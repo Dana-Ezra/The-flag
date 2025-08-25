0e94ff4c6116561d581547055014ad0e557a717c
Alice <ormanalis2007@gmail.com>
2025-08-25 11:16:18 +0300

    screen

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 import screen
+from screen import SCREEN
 from screen import default_window
+from game_field1 import create_matrix, flag_in_field
+import game_field1
 
 open_window=True
 def main():
     pygame.init()
-    default_window()
+    default_window(SCREEN)
+    open_window=True
     while open_window:
-        arrow_direction=pygame.key.get_pressed()
+        # arrow_direction=pygame.key.get_pressed()
 
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 open_window=False
-            elif kk
+            # elif kk
+    matrix=[]
+    create_matrix(matrix)
+    flag_in_field(matrix)
 
+main()
