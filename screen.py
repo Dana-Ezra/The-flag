e256835a6b5fb293b12abb5ad7627bb99667b805
Alice <ormanalis2007@gmail.com>
2025-08-25 10:54:19 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,22 +1,50 @@
+from math import floor
 import pygame
 import constants
 from constants import GREEN, GAME_FIELD
+import bush
 from pygame.examples.scrap_clipboard import screen
 
-screen= pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
-def default_window():
-    color=GREEN
+SCREEN = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
+def default_window(screen):
+    screen.fill(GREEN)
+    pygame.display.flip()
+    return screen
+
+from game_field1 import game_field
+from game_field1 import create_matrix
+
+def from_matrix_screen(screen):
+    dict_x = {}
+    dict_y = {}
+    for x in screen:
+        index_x = floor(x/10-1)
+        dict_x[x]=index_x
+    for y in screen:
+        index_y = floor(y/10-1)
+        dict_y[y]= index_y
+    return dict_x,dict_y
+
 def draw_bush():
-    bush= pygame.image.load("C:\ Users\jbt\Downloads\נספחים\נספחים\ bin\grass").convert()
-    from game_field import GAME_FIELD
     import random
+    bush_drawing= pygame.image.load("bush.py")
     for i in range(20):
-        random_row = random.randint(25)
-        random_col= random.randint(50)
+        random_row = random.randint(250)
+        random_col= random.randint(500)
         place=(random_row,random_col)
+        # for place[0]:
+
+        SCREEN.blits(bush_drawing, ())
+        pygame.display.update()
+
+
+
+
+
         #צריך לתרגם את המספר רנדום מאינדקס לקואורדינטות
         screen.blits(bush,place)
 
 # welcome_message=draw_massagee
 
 
+default_window(SCREEN)
