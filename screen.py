574a61c2dd7da8d6c578ee0e3d5d06015b4b2f12
Dana-Ezra <zinaezra@gmail.com>
2025-08-25 11:15:38 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,29 +1,41 @@
-from xml.etree.ElementPath import xpath_tokenizer
-
+from math import floor
 import pygame
 import constants
 from constants import GREEN, GAME_FIELD
+import bush
 from pygame.examples.scrap_clipboard import screen
 
 SCREEN = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
-def default_window(screen1):
+def default_window(screen):
     screen.fill(GREEN)
     pygame.display.flip()
-    return screen1
+    return screen
 
 from game_field1 import game_field
 from game_field1 import create_matrix
-from game_field1 import from_matrix_screen
+
+def from_matrix_screen(screen):
+    dict_x = {}
+    dict_y = {}
+    for x in screen:
+        index_x = floor(x/10-1)
+        dict_x[x]=index_x
+    for y in screen:
+        index_y = floor(y/10-1)
+        dict_y[y]= index_y
+    return dict_x,dict_y
 
 def draw_bush():
-    bush= pygame.image.load("C:\ Users\jbt\Downloads\נספחים\נספחים\ bin\grass").convert()
     import random
-
+    bush_drawing= pygame.image.load("bush.py")
     for i in range(20):
-        random_row = random.randint(25)
-        random_col= random.randint(50)
+        random_row = random.randint(250)
+        random_col= random.randint(500)
         place=(random_row,random_col)
-        for place[0]:
+        # for place[0]:
+
+        SCREEN.blits(bush_drawing, ())
+        pygame.display.update()
 
 
 
@@ -33,3 +45,6 @@ def draw_bush():
         screen.blits(bush,place)
 
 # welcome_message=draw_massagee
+
+
+default_window(SCREEN)
